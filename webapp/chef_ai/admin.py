603b699231aaecfba58aa32838bec428fc0da8dd
SshreyY <tup85783@temple.edu>
2025-04-06 21:29:26 -0400

    created the user history table that includes userId, selected ingredients, and generated recipe columns to store generated recipes

--- webapp/chef_ai/admin.py
+++ webapp/chef_ai/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Ingredient
-
+from .models import Ingredient, userHistory
 
 # Register your models here.
 admin.site.register(Ingredient)
+admin.site.register(userHistory)
